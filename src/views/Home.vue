bdc7d36964e450b10ab9f0d18b83aa8c8b231790
Matt Wright <mattwright3195@gmail.com>
2020-06-13 16:36:24 -0700

    router added

---
+++ src/views/Home.vue
@@ -0,0 +1,12 @@
+<template>
+  <div>Hello from Home</div>
+</template>
+
+<script lang="ts">
+export default {
+  name: "Home",
+  components: {},
+  props: {},
+  setup() {},
+};
+</script>
---
+++ src/views/Takeoff.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>Hello from Takeoff</div>
+  <!-- <div class="h-screen flex flex-col">
+    <div class="bg-gray-400 h-8 flex items-center justify-center">Empty</div>
+    <div class="flex flex-1 bg-gray-200 p-4">
+      <div class="flex h-full w-full bg-green-500 items-center justify-center">
+        <Canvas :data="data" />
+      </div>
+    </div>
+  </div>-->
+</template>
+
+<script lang="ts">
+export default {
+  name: "Takeoff",
+  components: {},
+  props: {},
+  setup() {
+    // const getRandomInt = (max) => Math.floor(Math.random() * Math.floor(max));
+    // const data = [];
+    // for (let i = 0; i < 1000; i++) {
+    //   data.push({ x: getRandomInt(10000), y: getRandomInt(10000) });
+    // }
+    // return { data };
+  },
+};
+</script>
